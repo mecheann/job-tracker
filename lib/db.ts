cc6863881186b9b3f76d38920cc6b5ad04aca440
Meche-Ann Natasia McKenzie <7514098+mecheann@users.noreply.github.com>
2026-04-07 14:06:35 -0500

    -created db connection function -added auth setup

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,48 @@
+import mongoose from "mongoose";
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof import("mongoose")> | null;
+}
+
+declare global {
+  var mongooseCache: MongooseCache;
+}
+
+let cached: MongooseCache = global.mongooseCache || {
+  conn: null,
+  promise: null,
+};
+
+if (!global.mongooseCache) {
+  global.mongooseCache = cached;
+}
+
+const dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+  return cached.conn;
+};
+
+export default dbConnect;

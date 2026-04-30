6c0d9083bfa98f52d64da9d13bcedbaeb20a15d2
Meche-Ann Natasia McKenzie <7514098+mecheann@users.noreply.github.com>
2026-04-30 01:44:14 -0500

    feat(model): added columnconfig , jobstatus enum

---
+++ lib/models/column-config.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export interface ColumnConfig {
+  color: string;
+  icon: ReactNode;
+}
---
+++ lib/models/job-status.ts
@@ -0,0 +1,7 @@
+export enum JobStatus {
+  Applied = "Applied",
+  UnderReview = "Under Review",
+  Interviewing = "Interviewing",
+  Offer = "Offer",
+  Rejected = "Rejected",
+}
